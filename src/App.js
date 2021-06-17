1cc0c4bffa44de50e173ae67409a515b27d0e80d
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-06-17 21:04:21 +0530

    added header component to the website

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
