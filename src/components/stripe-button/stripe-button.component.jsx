12c4908ad911647166a8d53fba8d7bff790796cd
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-07-08 18:10:22 +0530

    updating stripe integration and updating our favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./stripe-button.styles.scss";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey =
+    "pk_test_51JAuvnSDen84QJxZ1lAK8ibpLywIoeTb38vOQW2379AN7gQk9wpup0vrfxmhseZW7SnJVVyIXniJQ1g0DQDAoRtf0078HiUY8R";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      //   image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
